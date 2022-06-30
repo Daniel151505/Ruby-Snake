0cabaeff39e2b7694d738e0c27ec6df2b6570e07
Daniel151505 <daniel150520011@gmail.com>
2022-06-29 19:30:38 -0500

    structuring the project

---
+++ src/model/state.rb
@@ -0,0 +1,17 @@
+module Model
+    class Coord < Struct.new(row, col)  
+    end
+
+    class Food < Coord  
+    end
+
+    class Snake < Struct.new(positions)  
+    end
+
+    class Grid < Struct.new(row, col)
+    end
+
+    class State < Struct.new(snake, food, grid)
+    end
+
+end
