048a44aefbd2513762b4f7c4b74c67d76d4f4496
Daniel151505 <daniel150520011@gmail.com>
2022-06-30 19:32:23 -0500

    Programming snake movement

--- src/actions/actions.rb
+++ src/actions/actions.rb
@@ -40,4 +40,28 @@ module Actions
         end
     end
 
+    def position_is_valid?(state, next_position)
+        # verificar que este en la grilla
+        is_invalid = ((position.row >= state.grid.rows || 
+            position.row < 0) || 
+            (position.col >= state.grid.cols || 
+                position.col < 0))
+
+        return false if is_invalid
+
+        # verificar que no se este superponiendo a la serpiente
+        return !(state.snake.positions.include? position)
+        end
+    end
+
+    def move_snake_to(state, next_position)
+        new_positions = [next_position] + state.snake.positions[0...-1]
+        state.snake.positions = new_positions
+        state
+    end
+
+    def end_game(state)
+        state.game_finished = true
+        state
+    end
 end
