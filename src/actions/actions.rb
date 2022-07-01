6b032aa40b7c1707b95f344475c130e00e9eed52
Daniel151505 <daniel150520011@gmail.com>
2022-06-30 19:08:43 -0500

    Programming the basis for the movements

---
+++ src/actions/actions.rb
@@ -0,0 +1,43 @@
+module Actions
+    def self.move_snake(state)
+        next_direction = state.next_direction
+        next_position = calc_next_position(state)
+        # verificar que la siguiente casilla sea valida
+        if position_is_valid?(state, next_position)
+            move_snake_to(state, next_position)
+        else
+        # si no es valida --> terminar el juego
+        end_game(state)
+        end
+    end
+
+    private 
+
+    def calc_next_position(state)
+        curr_position = state.snake.positions.first
+        case state.next_direction
+        when UP
+            # decrementar fila
+            return Model::Coord.new(
+                    curr_position.row - 1,
+                    curr_position.col)
+        when RIGHT
+            # incrementar col
+            return Model::Coord.new(
+                curr_position.row,
+                curr_position.col + 1)
+        when DOWN
+            # incrementar fila
+            return Model::Coord.new(
+                curr_position.row + 1,
+                curr_position.col)
+        when LEFT
+            # decrementar col
+            return Model::Coord.new(
+                curr_position.row,
+                curr_position.col - 1)
+
+        end
+    end
+
+end
